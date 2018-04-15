b4e8e7ca4a243adaa66041d44700e43b9a7efa56
fiuzagr <fiuzagr@gmail.com>
2018-04-15 01:14:49 +0000

    Update

--- iaguara.sh
+++ iaguara.sh
@@ -5,17 +5,18 @@ rootPath=$(cd -P -- "$(dirname -- "${0}")" && printf '%s\n' "$(pwd -P)")
 export IAGUARA_HOME=${IAGUARA_HOME:-$rootPath}
 export IAGUARA_RC="${IAGUARA_HOME}/iaguara.sh"
 
-iaguara() {
-  local project="${1}"
-  local projectDir="${IAGUARA_HOME}/projects/${project}"
+projectsPath="${IAGUARA_HOME}/projects"
 
-  if [ -d "$projectDir" ]
+iaguara() {
+  local projectDir="${projectsPath}/${1}"
+
+  if [ -d "${projectDir}" ]
   then
     # remove first argument
     [ $# -ne 0 ] && shift
   else
-    project=base
+    projectDir="${projectsPath}/base"
   fi
 
-  env make -C ${projectDir} ${project} "${@}"
+  env make -C ${projectDir} "${@}"
 }
